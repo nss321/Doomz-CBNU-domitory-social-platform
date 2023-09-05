47619d14b4c702b37b1d37f9c8c4c1feba123183
Haena <ghkwlsspdl@naver.com>
2023-09-05 16:22:38 +0900

    [#001] ADD: UICOLOR확장 - titleColor 저장

---
+++ dormitoryFamiles/dormitoryFamiles/ExtenstionColor.swift
@@ -0,0 +1,13 @@
+//
+//  ExtenstionColor.swift
+//  dormitoryFamiles
+//
+//  Created by leehwajin on 2023/09/05.
+//
+
+
+import UIKit
+
+extension UIColor {
+    static let titleColor: UIColor =  #colorLiteral(red: 0.8296361566, green: 0.1152015552, blue: 0.3670018315, alpha: 1)
+}
---
+++ dormitoryFamiles/dormitoryFamiles/MealMenuViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  dormitoryFamiles
+//
+//  Created by leehwajin on 2023/09/04.
+//
+
+import UIKit
+
+class MealMenuViewController: UIViewController {
+    
+//    private lazy var schoolMealButton: UIButton = {
+//        let button = UIButton()
+//        button.setTitle("학식 보기", for: .normal)
+//        button.setTitleColor(UIColor.white, for: .normal)
+//        button.backgroundColor(UIColor.)
+//        //button.titleLabel?.font = UIFont(name: "SFProDisplay-Semibold", size: 18)
+//        return button
+//    }()
+//
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        self.view.addSubview(cancelButton)
+//        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            self.cancelButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+//            self.cancelButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+//            self.cancelButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+//            self.cancelButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+//        ])
+    }
+
+    
+}
