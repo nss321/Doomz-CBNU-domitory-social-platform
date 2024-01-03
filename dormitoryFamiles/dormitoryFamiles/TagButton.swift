753c16aff126ebc3f9ad8fb23b00864346602296
Haena <ghkwlsspdl@naver.com>
2024-01-03 20:07:30 +0900

    [#004] DISIGN: 커스텀 태그 버튼 생성 - 게시판 뷰컨트롤러에 태그 스크롤뷰 얹음

---
+++ dormitoryFamiles/dormitoryFamiles/TagButton.swift
@@ -0,0 +1,39 @@
+//
+//  TagButton.swift
+//  dormitoryFamiles
+//
+//  Created by leehwajin on 2024/01/03.
+//
+
+import UIKit
+
+class TagButton: UIButton {
+
+    override func setNeedsLayout() {
+        super.setNeedsLayout()
+        self.layer.cornerRadius = self.bounds.height / 2
+    }
+    
+    convenience init(title: String) {
+        self.init()
+        setUI(title: title)
+    }
+    
+    private func setUI(title: String) {
+        self.setTitle(title, for: .normal)
+        self.layer.borderColor = UIColor.lightGray.cgColor
+        self.layer.borderWidth = 1
+        self.layer.masksToBounds = true
+        self.contentEdgeInsets = UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16)
+        self.setTitleColor(.black, for: .normal)
+    }
+    
+    func changeWhiteColor() {
+        self.backgroundColor = .white
+    }
+
+    func changeOrangeColor() {
+        self.backgroundColor = .black
+    }
+
+}
--- dormitoryFamiles/dormitoryFamiles/bulletinBoardMainViewController.swift
+++ dormitoryFamiles/dormitoryFamiles/bulletinBoardMainViewController.swift
@@ -9,10 +9,15 @@ import UIKit
 
 class BulletinBoardMainViewController: UIViewController {
     let cellIdentifier = "BulletinBoardMainTableViewCell"
+    let tagScrollView = TagScrollView()
+    var tags = ["도와주세요","함께해요","나눔해요","궁금해요","분실신고"]
+    @IBOutlet weak var naviCustomView: UIView!
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         setTableViewCell()
+        setTag()
+        getTagMakeButton()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -22,10 +27,84 @@ class BulletinBoardMainViewController: UIViewController {
     
     private func setTableViewCell() {
         tableView.dataSource = self
-               let nib = UINib(nibName: cellIdentifier, bundle: nil)
+        let nib = UINib(nibName: cellIdentifier, bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: cellIdentifier)
     }
     
+    private func setTag() {
+        //네비게이션
+        self.navigationItem.title = "관심 목록"
+        let allTagButton = TagButton(title: "전체")
+        tagScrollView.tagStackView.addArrangedSubview(allTagButton)
+        allTagButton.backgroundColor = UIColor.black
+        allTagButton.setTitleColor(UIColor.white, for: .normal)
+        allTagButton.layer.borderWidth = 0
+        tagLayout()
+        allTagButton.addTarget(nil, action: #selector(categoryButtonTapped), for: .touchUpInside)
+    }
+    
+    private func tagLayout() {
+        self.view.addSubview(tagScrollView)
+        tagScrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let height: CGFloat = self.view.frame.height
+        let width: CGFloat = self.view.frame.width
+        let figmaHeight: CGFloat = 852
+        let figmaWidth: CGFloat = 391
+        let heightRatio: CGFloat = height/figmaHeight
+        let widthRatio: CGFloat = width/figmaWidth
+        NSLayoutConstraint.activate([
+            tagScrollView.bottomAnchor.constraint(equalTo: self.naviCustomView.bottomAnchor, constant: -10*heightRatio),
+            tagScrollView.leadingAnchor.constraint(equalTo: self.naviCustomView.leadingAnchor, constant: 16*widthRatio),
+            tagScrollView.trailingAnchor.constraint(equalTo: self.naviCustomView.trailingAnchor, constant: -16*widthRatio),
+            tagScrollView.heightAnchor.constraint(equalToConstant: 32*heightRatio)
+        ])
+    }
+    
+    private func makeButton(tag: String)  {
+        let tagButton = TagButton(title: tag)
+        tagScrollView.tagStackView.addArrangedSubview(tagButton)
+        tagButton.addTarget(self, action: #selector(categoryButtonTapped(_:)), for: .touchUpInside)
+        tagButton.tag = 4
+    }
+    
+    @objc private func categoryButtonTapped(_ sender: TagButton) {
+        
+        //(버튼)활성화UI를 비활성화UI로
+        for case let button as TagButton in tagScrollView.tagStackView.arrangedSubviews {
+            if button.backgroundColor == .black {
+                button.changeWhiteColor()
+                button.setTitleColor(.black, for: .normal)
+                button.layer.borderWidth = 1
+            }
+        }
+        
+        guard sender.currentTitle != "전체" else {
+            sender.changeOrangeColor()
+            sender.setTitleColor(UIColor.white, for: .normal)
+            sender.layer.borderWidth = 0
+            //page = 0
+            // fetchItemList(page: page)
+            return
+        }
+        
+        //(버튼)비활성화UI를 활성화UI로
+        //categoryNumber = sender.tag
+        sender.changeOrangeColor()
+        sender.setTitleColor(UIColor.white, for: .normal)
+        sender.layer.borderWidth = 0
+        
+        //해당 상품리스트가 조회되도록
+        //page = 0
+        //fetchItemList(page: page, categoryNumber: categoryNumber)
+    }
+    
+    private func getTagMakeButton() {
+        //찜했던 상품들이 해당하는 카테고리를 버튼으로 만듬
+        tags.forEach { tag in
+            self.makeButton(tag: tag)
+        }
+    }
 }
 
 extension BulletinBoardMainViewController: UITableViewDelegate, UITableViewDataSource {
@@ -34,8 +113,8 @@ extension BulletinBoardMainViewController: UITableViewDelegate, UITableViewDataS
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
-            return cell
-        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        return cell
+    }
 }
 
