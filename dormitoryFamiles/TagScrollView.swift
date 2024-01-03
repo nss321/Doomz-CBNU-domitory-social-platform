312fda1ab47c9d840a4355d59a597a88b73b0686
Haena <ghkwlsspdl@naver.com>
2024-01-03 19:34:21 +0900

    [#004] DESIGN: 태그스크롤뷰 커스텀

---
+++ dormitoryFamiles/TagScrollView.swift
@@ -0,0 +1,40 @@
+//
+//  TagScrollView.swift
+//  dormitoryFamiles
+//
+//  Created by leehwajin on 2024/01/03.
+//
+
+import UIKit
+
+class TagScrollView: UIScrollView {
+
+    var tagStackView = UIStackView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        tagStackView.spacing = 4
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        tagStackView.spacing = 4
+        configure()
+    }
+    
+    private func configure() {
+        self.addSubview(tagStackView)
+        tagStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tagStackView.heightAnchor.constraint(equalTo: self.heightAnchor),
+            tagStackView.topAnchor.constraint(equalTo: self.topAnchor),
+            tagStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            tagStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            tagStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+        //스크롤바 가리기
+        self.showsHorizontalScrollIndicator = false
+    }
+
+}
