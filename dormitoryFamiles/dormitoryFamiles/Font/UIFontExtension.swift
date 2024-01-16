1c56f559ce29327997aaba9cd0e46db0cef2c080
Haena <ghkwlsspdl@naver.com>
2024-01-16 19:53:55 +0900

    [#004] ADD: FontPretendardGOVVariable폰트 설정 - @IBDesignable처리 (스토리보드로 바로 사용할 수 있도록) - 커스텀 폰트 제작 - 오타방지를 위한 enum FontPretendardGOVVariable 세팅

---
+++ dormitoryFamiles/dormitoryFamiles/Font/UIFontExtension.swift
@@ -0,0 +1,123 @@
+//
+//  UIFontExtension.swift
+//  dormitoryFamiles
+//
+//  Created by leehwajin on 2024/01/16.
+//
+
+import UIKit
+
+@IBDesignable
+extension UILabel {
+    @IBInspectable var title1: String {
+        get {
+            return self.font.fontName
+        }
+        set {
+            setAttributedFont(name: FontPretendardGOVVariable.bold.rawValue, size: 20, text: newValue)
+        }
+    }
+    
+    @IBInspectable var title2: String {
+        get {
+            return self.font.fontName
+        }
+        set {
+            setAttributedFont(name: FontPretendardGOVVariable.bold.rawValue, size: 18, text: newValue)
+        }
+    }
+    
+    @IBInspectable var title3: String {
+        get {
+            return self.font.fontName
+        }
+        set {
+            setAttributedFont(name: FontPretendardGOVVariable.semiBold.rawValue, size: 18, text: newValue)
+        }
+    }
+    
+    @IBInspectable var title4: String {
+        get {
+            return self.font.fontName
+        }
+        set {
+            setAttributedFont(name: FontPretendardGOVVariable.bold.rawValue, size: 16, text: newValue)
+        }
+    }
+    
+    @IBInspectable var title5: String {
+        get {
+            return self.font.fontName
+        }
+        set {
+            setAttributedFont(name: FontPretendardGOVVariable.semiBold.rawValue, size: 16, text: newValue)
+        }
+    }
+    
+    @IBInspectable var subTitle1: String {
+        get {
+            return self.font.fontName
+        }
+        set {
+            setAttributedFont(name: FontPretendardGOVVariable.medium.rawValue, size: 16, text: newValue)
+        }
+    }
+    
+    @IBInspectable var subTitle2: String {
+        get {
+            return self.font.fontName
+        }
+        set {
+            setAttributedFont(name: FontPretendardGOVVariable.medium.rawValue, size: 14, text: newValue)
+        }
+    }
+    
+    @IBInspectable var body1: String {
+        get {
+            return self.font.fontName
+        }
+        set {
+            setAttributedFont(name: FontPretendardGOVVariable.regular.rawValue, size: 16, text: newValue)
+        }
+    }
+    
+    @IBInspectable var body2: String {
+        get {
+            return self.font.fontName
+        }
+        set {
+            setAttributedFont(name: FontPretendardGOVVariable.regular.rawValue, size: 14, text: newValue)
+        }
+    }
+    
+    @IBInspectable var button: String {
+        get {
+            return self.font.fontName
+        }
+        set {
+            setAttributedFont(name: FontPretendardGOVVariable.medium.rawValue, size: 14, text: newValue)
+        }
+    }
+    
+    private func setAttributedFont(name: String, size: CGFloat, text: String) {
+        if let newFont = UIFont(name: name, size: size) {
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.lineSpacing = newFont.pointSize * 0.5 // 행간 150%
+            
+            let attributedString = NSAttributedString(string: text, attributes: [
+                .font: newFont,
+                .kern: -newFont.pointSize * 0.02, // 자간 -2%
+                .paragraphStyle: paragraphStyle
+            ])
+            
+            self.attributedText = attributedString
+        }
+    }
+}
+
+enum FontPretendardGOVVariable: String {
+    case regular = "PretendardGOVVariable-Regular"
+    case medium = "PretendardGOVVariable-Medium"
+    case semiBold = "PretendardGOVVariable-SemiBold"
+    case bold = "PretendardGOVVariable-Bold"
+}
--- dormitoryFamiles/dormitoryFamiles/HomeViewController.swift
+++ dormitoryFamiles/dormitoryFamiles/HomeViewController.swift
@@ -15,14 +15,23 @@ class HomeViewController: UIViewController {
     
     @IBOutlet weak var timeLabel: UILabel!
     
+    @IBOutlet weak var todayMenuLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.menuLabel.sizeToFit()
         let stackViewBottomConstraint = timeLabel.bottomAnchor.constraint(equalTo: lineView.bottomAnchor, constant: -16)
         stackViewBottomConstraint.isActive = true
         
         self.navigationController?.setNavigationBarHidden(true, animated: true)
+        
+        for family in UIFont.familyNames {
+            print(family)
+            for name in UIFont.fontNames(forFamilyName: family) {
+                print(name)
+            }
+        }
         
     }
     
