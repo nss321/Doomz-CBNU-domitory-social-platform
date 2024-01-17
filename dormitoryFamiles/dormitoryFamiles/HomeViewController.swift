3103c31371c435fa7be3ffc0c540c3f5ac35b62c
Haena <ghkwlsspdl@naver.com>
2024-01-18 01:17:59 +0900

    [#004] FEAT: 액션시트 선택시 기숙사레이블 텍스트 변경 - 스토리보드를 커스텀하여 폰트를 적용중이라, setTitle이 아닌 button.스토리보드의 커스텀 폰트이름 = "원하는 타이틀명" 으로 설정

--- dormitoryFamiles/dormitoryFamiles/HomeViewController.swift
+++ dormitoryFamiles/dormitoryFamiles/HomeViewController.swift
@@ -34,18 +34,18 @@ class HomeViewController: UIViewController {
         self.menuLabel.textAlignment = .center
         
         setDormitoryButton()
-       
+        
         
         let stackViewBottomConstraint = timeLabel.bottomAnchor.constraint(equalTo: lineView.bottomAnchor, constant: -16)
         stackViewBottomConstraint.isActive = true
         
         self.navigationController?.setNavigationBarHidden(true, animated: true)
-//        for family in UIFont.familyNames {
-//            print(family)
-//            for name in UIFont.fontNames(forFamilyName: family) {
-//                print(name)
-//            }
-//        }
+        //        for family in UIFont.familyNames {
+        //            print(family)
+        //            for name in UIFont.fontNames(forFamilyName: family) {
+        //                print(name)
+        //            }
+        //        }
         
     }
     
@@ -69,16 +69,40 @@ class HomeViewController: UIViewController {
         sender.tintColor = .primary
     }
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func dormitoryButtonTapped(_ sender: UIButton) {
+        
+        let alert = UIAlertController(title: "", message: "기숙사 선택", preferredStyle: .actionSheet)
+        
+        let gaesungjae = UIAlertAction(title: "개성재", style: .default, handler: {_ in
+            self.dormitoryButton.head2 = "개성재"
+        })
+        let yangsungjae = UIAlertAction(title: "양성재", style: .default, handler: {_ in
+            self.dormitoryButton.head2 = "양성재"
+        })
+        let yangjinjae = UIAlertAction(title: "양진재", style: .default, handler: {_ in
+            self.dormitoryButton.head2 = "양진재"
+        })
+        
+        
+        alert.addAction(gaesungjae)
+        alert.addAction(yangsungjae)
+        alert.addAction(yangjinjae)
+        
+        
+        present(alert, animated: true, completion: nil)
+        
     }
-    */
-
+    
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
