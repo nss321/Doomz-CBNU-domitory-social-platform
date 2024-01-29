8ee717bd3bba96832b0c4a60aed6f3a3a1b93fb3
Haena <ghkwlsspdl@naver.com>
2024-01-29 19:08:05 +0900

    [#007] DESIGN: 컬렉션뷰셀 xib레이아웃 세팅

---
+++ dormitoryFamiles/dormitoryFamiles/BulluetinBoardCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  BulluetinBoardCollectionViewCell.swift
+//  dormitoryFamiles
+//
+//  Created by leehwajin on 2024/01/27.
+//
+
+import UIKit
+
+class BulluetinBoardCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
