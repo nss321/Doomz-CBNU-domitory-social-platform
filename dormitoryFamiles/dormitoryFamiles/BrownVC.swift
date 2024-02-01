4a2019027b0cf0a973b5062d3ad9dfa7dc6fbc5a
Haena <ghkwlsspdl@naver.com>
2024-02-02 00:05:28 +0900

    [#007] BUG: 컨테이너뷰 시도

--- dormitoryFamiles/dormitoryFamiles/BrownVC.swift
+++ dormitoryFamiles/dormitoryFamiles/BrownVC.swift
@@ -8,22 +8,71 @@
 import UIKit
 
 class BrownVC: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setDelegate()
+        self.collectionView.register(UINib(nibName: "BulluetinBoardCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "cell")
+
+        collectionView.register(UINib(nibName: "PopularCollectionViewHeader",
+                                      bundle: nil),
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: "header")
+
     }
     
+        private func setDelegate() {
+            self.collectionView.delegate = self
+            self.collectionView.dataSource = self
+        }
+    
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+extension BrownVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 99
     }
-    */
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as UICollectionViewCell
+        cell.layer.borderWidth = 1
+        cell.layer.borderColor = UIColor(red: 0.894, green: 0.898, blue: 0.906, alpha: 1).cgColor
+
+        cell.layer.cornerRadius = 20
+
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 335, height: 167)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 12
+    }
+
+    // 헤더의 크기를 지정하는 함수
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: 10, height: 56)
+    }
+
+    // 헤더를 생성하는 함수
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath)
+            // headerView 설정 코드를 여기에 작성하세요
+
+            return headerView
+        default:
+            assert(false, "Invalid element type")
+        }
+    }
 
 }
--- dormitoryFamiles/dormitoryFamiles/BulletinBoardMainViewController.swift
+++ dormitoryFamiles/dormitoryFamiles/BulletinBoardMainViewController.swift
@@ -12,7 +12,9 @@ class BulletinBoardMainViewController: TabmanViewController {
     private var viewControllers: [UIViewController] {
         let brownVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "brownVC") as! BrownVC
         let yellowVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "yellowVC") as! YellowVC
-        return [brownVC, yellowVC, UIViewController(), UIViewController(), UIViewController()]
+        let aaa = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "aaa")
+        //let aaa = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "aaa")
+        return [brownVC, yellowVC, aaa, UIViewController(), UIViewController()]
     }
     @IBOutlet weak var naviCustomView: UIView!
     
@@ -24,15 +26,15 @@ class BulletinBoardMainViewController: TabmanViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
-        setDelegate()
+        //setDelegate()
         setTapman()
-        self.collectionView.register(UINib(nibName: "BulluetinBoardCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "cell")
-        
-        collectionView.register(UINib(nibName: "PopularCollectionViewHeader",
-                                      bundle: nil),
-                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
-                                withReuseIdentifier: "header")
-        
+        //self.collectionView.register(UINib(nibName: "BulluetinBoardCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "cell")
+//
+//        collectionView.register(UINib(nibName: "PopularCollectionViewHeader",
+//                                      bundle: nil),
+//                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+//                                withReuseIdentifier: "header")
+//
         
         
         
@@ -50,10 +52,10 @@ class BulletinBoardMainViewController: TabmanViewController {
         self.navigationController?.setNavigationBarHidden(false, animated: false)
     }
     
-    private func setDelegate() {
-        self.collectionView.delegate = self
-        self.collectionView.dataSource = self
-    }
+//    private func setDelegate() {
+//        self.collectionView.delegate = self
+//        self.collectionView.dataSource = self
+//    }
     
     private func setUI() {
         writeButton.configuration?.image = UIImage(named: "bulletinBoardPlus")
@@ -89,49 +91,49 @@ class BulletinBoardMainViewController: TabmanViewController {
     
 }
 
-extension BulletinBoardMainViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 99
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as UICollectionViewCell
-        cell.layer.borderWidth = 1
-        cell.layer.borderColor = UIColor(red: 0.894, green: 0.898, blue: 0.906, alpha: 1).cgColor
-        
-        cell.layer.cornerRadius = 20
-        
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 335, height: 167)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 12
-    }
-    
-    // 헤더의 크기를 지정하는 함수
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return CGSize(width: 10, height: 56)
-    }
-    
-    // 헤더를 생성하는 함수
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        switch kind {
-        case UICollectionView.elementKindSectionHeader:
-            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath)
-            // headerView 설정 코드를 여기에 작성하세요
-            
-            return headerView
-        default:
-            assert(false, "Invalid element type")
-        }
-    }
-    
-}
+//extension BulletinBoardMainViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+//
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 99
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as UICollectionViewCell
+//        cell.layer.borderWidth = 1
+//        cell.layer.borderColor = UIColor(red: 0.894, green: 0.898, blue: 0.906, alpha: 1).cgColor
+//
+//        cell.layer.cornerRadius = 20
+//
+//        return cell
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: 335, height: 167)
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 12
+//    }
+//
+//    // 헤더의 크기를 지정하는 함수
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+//        return CGSize(width: 10, height: 56)
+//    }
+//
+//    // 헤더를 생성하는 함수
+//    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+//        switch kind {
+//        case UICollectionView.elementKindSectionHeader:
+//            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath)
+//            // headerView 설정 코드를 여기에 작성하세요
+//
+//            return headerView
+//        default:
+//            assert(false, "Invalid element type")
+//        }
+//    }
+//
+//}
 
 
 extension BulletinBoardMainViewController: PageboyViewControllerDataSource, TMBarDataSource {
