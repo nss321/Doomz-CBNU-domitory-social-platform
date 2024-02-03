49a59903795b2e984f04cfe43e090465d1f69731
Haena <ghkwlsspdl@naver.com>
2024-02-03 16:58:45 +0900

    [#008] FEAT: textFieldDelegate설정 - 델리게이트 세팅 - 확장시켜 텍스트필드 델리게이트 채택 - 20자 제한 적용중

--- dormitoryFamiles/dormitoryFamiles/registerPostViewController.swift
+++ dormitoryFamiles/dormitoryFamiles/registerPostViewController.swift
@@ -16,20 +16,29 @@ class registerPostViewController: UIViewController {
     
     @IBOutlet weak var dormitoryButton: UIButton!
     
+    
+    @IBOutlet weak var countTextFieldTextLabel: UILabel!
+    
     let dropDown = DropDown()
     
     override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-            setNavigationBar()
-        setDropDown()
-        }
+        super.viewWillAppear(animated)
+        setNavigationBar()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setDropDown()
+        setDelegate()
+        setTextField()
     }
     
     private func setNavigationBar() {
         self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    private func setDelegate() {
+        textField.delegate = self
     }
     
     private func setDropDown() {
@@ -40,7 +49,11 @@ class registerPostViewController: UIViewController {
         DropDown.appearance().shadowOpacity = 0
         DropDown.appearance().selectionBackgroundColor = .gray0 ?? .white
         DropDown.appearance().textFont = UIFont(name: CustomFonts.defult.rawValue, size: 16)!
-
+        
+    }
+    
+    private func setTextField() {
+        countTextFieldTextLabel.text = String(textField.text!.count) + "/20"
     }
     
     @IBAction func dropDownButtonTapped(_ sender: UIButton) {
@@ -59,10 +72,42 @@ class registerPostViewController: UIViewController {
         dropDown.bottomOffset = CGPoint(x: 0, y:((dropDown.anchorView?.plainView.bounds.height)!-5))
         sender.borderColor = .primaryMid
         dropDown.show()
-        dropDown.selectionAction = { [unowned self] (index: Int, item: String) in
+        dropDown.selectionAction = { (index: Int, item: String) in
             sender.setTitle(item, for: .normal)
             sender.borderColor = .gray1
         }
     }
+}
+
+extension registerPostViewController: UITextFieldDelegate {
+//    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+//        if textField.text.count == 20 {
+//            return false
+//        }
+//    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+    }
+    
+    
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        //백스페이스는 언제나 true
+        if string.isEmpty {
+            countTextFieldTextLabel.text = String(textField.text!.count) + "/20"
+            return true
+        }
+        
+        //20글자가 넘어가면 false
+        if textField.text!.count == 20 {
+            return false
+        }
+        
+        
+        countTextFieldTextLabel.text = String(textField.text!.count + string.count) + "/20"
+        return true
+    }
+    
     
 }
