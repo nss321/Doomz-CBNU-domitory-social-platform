0782e74de4bc31fe6e4709a98cff92970505c48d
Haena <ghkwlsspdl@naver.com>
2024-02-03 16:25:28 +0900

    [#008] DESIGN: 글쓰기 UI에 DropDown추가 - dropDown의 앞 엣지 추가 - 기숙사 버튼/카테고리 버튼 클릭시 각각에 맞는 드롭다운 세팅 - 드롭다운 클릭 시 버튼의 title변경

--- dormitoryFamiles/dormitoryFamiles/registerPostViewController.swift
+++ dormitoryFamiles/dormitoryFamiles/registerPostViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import DropDown
 
 class registerPostViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     
+    @IBOutlet weak var categoryButton: UIButton!
+    
+    @IBOutlet weak var dormitoryButton: UIButton!
+    
+    let dropDown = DropDown()
+    
     override func viewWillAppear(_ animated: Bool) {
             super.viewWillAppear(animated)
             setNavigationBar()
+        setDropDown()
         }
     
     override func viewDidLoad() {
@@ -24,6 +32,37 @@ class registerPostViewController: UIViewController {
         self.navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
-   
+    private func setDropDown() {
+        DropDown.startListeningToKeyboard()
+        DropDown.appearance().setupCornerRadius(20)
+        DropDown.appearance().backgroundColor = .white
+        DropDown.appearance().cellHeight = 52
+        DropDown.appearance().shadowOpacity = 0
+        DropDown.appearance().selectionBackgroundColor = .gray0 ?? .white
+        DropDown.appearance().textFont = UIFont(name: CustomFonts.defult.rawValue, size: 16)!
 
+    }
+    
+    @IBAction func dropDownButtonTapped(_ sender: UIButton) {
+        switch sender {
+        case dormitoryButton:
+            //TODO: 기숙사 종류 넣어야함
+            dropDown.dataSource = ["개성재", "양성재","양진재"]
+        case categoryButton:
+            dropDown.dataSource = ["도와주세요", "함께해요", "나눔해요", "분실신고"]
+        default:
+            dropDown.dataSource = []
+        }
+        
+        //공통된 작업
+        dropDown.anchorView = sender
+        dropDown.bottomOffset = CGPoint(x: 0, y:((dropDown.anchorView?.plainView.bounds.height)!-5))
+        sender.borderColor = .primaryMid
+        dropDown.show()
+        dropDown.selectionAction = { [unowned self] (index: Int, item: String) in
+            sender.setTitle(item, for: .normal)
+            sender.borderColor = .gray1
+        }
+    }
+    
 }
