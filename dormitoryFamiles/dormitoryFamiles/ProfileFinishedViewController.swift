ee7d04bde10d73b28e7733b88ea29faa9df4ee35
Haena <ghkwlsspdl@naver.com>
2024-02-20 20:19:34 +0900

    [#005] FEAT: 프로필설정 학생증인증UI, 학생증 등록완료UI - 스토리보드로 작업 - label의 얼라이먼트가 안잡히는 문제로, 학생증 등록완료UI의 설명라벨은 코드로 얼라이먼트를 센터로 맞춰줌

---
+++ dormitoryFamiles/dormitoryFamiles/ProfileFinishedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileFinishedViewController.swift
+//  dormitoryFamiles
+//
+//  Created by leehwajin on 2024/02/20.
+//
+
+import UIKit
+
+class ProfileFinishedViewController: UIViewController {
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        descriptionLabel.textAlignment = .center
+    }
+   
+}
