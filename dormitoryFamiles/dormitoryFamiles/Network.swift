719623201945e675a49bf328d4fc494a79afad3e
Haena <ghkwlsspdl@naver.com>
2024-02-26 19:19:24 +0900

    [#010] ADD: Dormitory열거형 선언 - rawValue String처리함

--- dormitoryFamiles/dormitoryFamiles/Network.swift
+++ dormitoryFamiles/dormitoryFamiles/Network.swift
@@ -8,4 +8,17 @@
 import Foundation
 struct Network {
     let url = "http://43.202.254.127:8080"
+    
+    
+    
+    func getAllPost(dormitory: Dormitory) {
+        let url = url + "/api/dormitories/\(dormitory.rawValue)/articles"
+    }
+}
+
+enum Dormitory: String {
+    case 본관 = "본관"
+    case 양성재 = "양성재"
+    case 양진재 = "양진재"
+    case 양현재 = "양현재"
 }
