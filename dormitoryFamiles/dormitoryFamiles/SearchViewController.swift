ec165bafdfa0af52594de07547fbe8cc3da6c619
Haena <ghkwlsspdl@naver.com>
2024-03-05 00:11:26 +0900

    [#011] FEAT: 검색결과 컬렉션뷰 셀을 클릭시 해당 디테일뷰로 이동

--- dormitoryFamiles/dormitoryFamiles/SearchViewController.swift
+++ dormitoryFamiles/dormitoryFamiles/SearchViewController.swift
@@ -100,15 +100,16 @@ extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSo
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
         let articleElement = articles[indexPath.row]
         
         let id = articleElement.articleId
         
-        let url = "http://43.202.254.127:8080/api/articles/{articleId}"
-        let articleDetailViewController = BulletinBoardDetailViewViewController()
-        
-        self.navigationController?.pushViewController(articleDetailViewController, animated: true)
+        let url = "http://43.202.254.127:8080/api/articles/\(id)"
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            if let articleDetailViewController = storyboard.instantiateViewController(withIdentifier: "detail") as? BulletinBoardDetailViewViewController {
+                articleDetailViewController.setUrl(url: url)
+                self.navigationController?.pushViewController(articleDetailViewController, animated: true)
+            }
     }
     
     
