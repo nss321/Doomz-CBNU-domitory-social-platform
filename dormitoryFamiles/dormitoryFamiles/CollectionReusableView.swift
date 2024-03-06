9c777b6d1fd1d3cbe057cbfdb8ab33eb9f62d3c2
Haena <ghkwlsspdl@naver.com>
2024-03-07 00:59:50 +0900

    [#009] DESIGN: 대댓글 컬렉션뷰 헤더 XIB세팅

---
+++ dormitoryFamiles/dormitoryFamiles/CollectionReusableView.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionReusableView.swift
+//  dormitoryFamiles
+//
+//  Created by leehwajin on 2024/03/07.
+//
+
+import UIKit
+
+class CollectionReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
---
+++ dormitoryFamiles/dormitoryFamiles/ReplyHeaderCollectionReusableView.swift
@@ -0,0 +1,17 @@
+//
+//  ReplyHeaderCollectionReusableView.swift
+//  dormitoryFamiles
+//
+//  Created by leehwajin on 2024/03/07.
+//
+
+import UIKit
+
+class ReplyHeaderCollectionReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
