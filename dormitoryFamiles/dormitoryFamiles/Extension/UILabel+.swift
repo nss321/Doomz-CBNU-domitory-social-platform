19230c63b2a469beaa326702a70f61f7c4816d7e
BAE <lexon1209@gmail.com>
2024-03-17 06:26:13 +0900

    [FIX] 버그 픽스

--- dormitoryFamiles/dormitoryFamiles/Extension/UILabel+.swift
+++ dormitoryFamiles/dormitoryFamiles/Extension/UILabel+.swift
@@ -11,7 +11,7 @@ extension UILabel {
     func addCharacterSpacing(kernalValue:Double = -0.02) {
         guard let text = text, !text.isEmpty else { return }
         let string = NSMutableAttributedString(string: text)
-        string.addAttribute(NSAttributedString.Key.kern, value: kernalValue, range: NSRange(location: 0, length: string.length -1))
+        string.addAttribute(NSAttributedString.Key.kern, value: kernalValue, range: NSRange(location: 0, length: string.length-1))
         attributedText = string
     }
 }
--- dormitoryFamiles/dormitoryFamiles/Matching Option Setting/ViewController/SleepPatternViewController.swift
+++ dormitoryFamiles/dormitoryFamiles/Matching Option Setting/ViewController/SleepPatternViewController.swift
@@ -6,15 +6,15 @@
 //
 
 import UIKit
-import Snapkit
+import SnapKit
 
 final class SleepPatternViewController: UIViewController {
     
     private let contentLabel: UILabel = {
         let label = UILabel()
         label.text = "나의 수면 패턴은?"
-        label.font = FontManager.body()
-        label.textColor = .gs10
+        label.font = FontManager.title2()
+        label.textColor = .black
         label.numberOfLines = 0
         label.lineBreakMode = .byCharWrapping
         
@@ -25,24 +25,12 @@ final class SleepPatternViewController: UIViewController {
         let label = UILabel()
         label.text = "긱사생활 설정"
         label.font = FontManager.head1()
-        label.textColor = .gs20
+        label.textColor = .black
         
         return label
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    func setupNavigationBar() {
-        view.addSubview(naviLine)
-        naviLine.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(106)
-            $0.left.right.equalToSuperview()
-            $0.height.equalTo(0.33)
-        }
-        self.navigationController?.navigationBar.tintColor = .gs20
-        self.navigationItem.titleView = self.navigationTitle
-        self.navigationItem.leftBarButtonItem = self.leftBarButtonItem
     }
 }
