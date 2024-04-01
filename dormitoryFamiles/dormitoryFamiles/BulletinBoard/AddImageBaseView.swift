04d3797358126a98b39a5d6f568722dac1603ab3
Haena <ghkwlsspdl@naver.com>
2024-04-01 22:45:16 +0900

    [#016] REFACTOR: AddImageBaseView생성
    - 이미지들의 index를 체킹하기 위해 한곳에 뭉치려고 생성

---
+++ dormitoryFamiles/dormitoryFamiles/BulletinBoard/AddImageBaseView.swift
@@ -0,0 +1,20 @@
+//
+//  addImageBaseView.swift
+//  dormitoryFamiles
+//
+//  Created by leehwajin on 2024/04/01.
+//
+
+import UIKit
+
+class addImageBaseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
