b359c93c63240eb7f80113c269cbe2905788bec7
Haena <ghkwlsspdl@naver.com>
2024-05-02 15:54:48 +0900

    ignore전 데이터 삭제
    - 데이터 삭제 후 ignore처리를 위함

--- dormitoryFamiles/dormitoryFamiles/Network/Token.swift
+++ dormitoryFamiles/dormitoryFamiles/Network/Token.swift
@@ -9,7 +9,7 @@ import Foundation
 
 final class Token {
     static let shared = Token()
-    var number = "eyJraWQiOiJrZXkxIiwiYWxnIjoiSFMzODQifQ.eyJzdWIiOiJkbmxnaWQxMTlAZGF1bS5uZXQiLCJpYXQiOjE3MTI1NzYxMzYsImV4cCI6MTcxMzE3NjEzNn0.cflm0JHVRRCHYYUsIl37PSh4936afRnj_7i3Z65BOlc_0EXIU9PYGJmt9oizlWqH"
+    var number = ""
     
     private init() { }
 }
