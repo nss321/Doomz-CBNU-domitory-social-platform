361083b00c6dcfc2ff72aceca309b0e890636e4a
Haena <ghkwlsspdl@naver.com>
2024-06-19 14:57:50 +0900

    [#47] ADD: MealMenuViewController 생성

--- dormitoryFamiles/dormitoryFamiles/Home/MealMenuViewController.swift
+++ dormitoryFamiles/dormitoryFamiles/Home/MealMenuViewController.swift
@@ -6,31 +6,14 @@
 //
 
 import UIKit
-import WebKit
 
 final class MealMenuViewController: UIViewController {
     
-    private lazy var schoolMealButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("학식 보기", for: .normal)
-        button.setTitleColor(UIColor.white, for: .normal)
-        button.backgroundColor = UIColor.titleColor
-        button.titleLabel?.font = UIFont(name: "SFProDisplay-Semibold", size: 18)
-        return button
-    }()
-
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.view.addSubview(schoolMealButton)
-        schoolMealButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            self.schoolMealButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-            self.schoolMealButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
-            self.schoolMealButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
-            self.schoolMealButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
-        ])
+      
     }
     
 }
