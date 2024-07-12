01de0532f5031e8191d4d098a7258a8c19e018e6
Haena <ghkwlsspdl@naver.com>
2024-07-12 17:54:27 +0900

    [#63] ADD: 채팅홈 뷰컨트롤러 생성
    - 스토리보드와 연결
    - 스냅킷 import

---
+++ dormitoryFamiles/dormitoryFamiles/chat/chattingHomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  chattingHomeViewController.swift
+//  dormitoryFamiles
+//
+//  Created by leehwajin on 2024/07/12.
+//
+
+import UIKit
+import SnapKit
+
+class chattingHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
