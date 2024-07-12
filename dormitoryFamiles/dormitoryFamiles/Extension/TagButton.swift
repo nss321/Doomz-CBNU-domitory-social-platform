84b49cee0e865b9edb156f477a1887d9ba7169ca
Haena <ghkwlsspdl@naver.com>
2024-07-12 21:33:56 +0900

    [#63] ADD: 커스텀 UIButton인 PrimaryButton생성
    - 전체보기, 더보기라 title을 가진 Primary색상인 버튼이 자주 쓰여 제작

--- dormitoryFamiles/dormitoryFamiles/Extension/TagButton.swift
+++ dormitoryFamiles/dormitoryFamiles/Extension/TagButton.swift
@@ -47,7 +47,7 @@ class RoundButton: UIButton {
 }
 
 class TagButton: RoundButton {
-
+    
     override func setNeedsLayout() {
         super.setNeedsLayout()
         self.layer.cornerRadius = min(self.bounds.height, 48) / 2
@@ -70,12 +70,37 @@ class TagButton: RoundButton {
     private func changePinkColor() {
         self.backgroundColor = .primaryMid
     }
-
+    
     private func changeWhiteColor() {
         self.backgroundColor = .white
     }
     
     private func changeTitleGray() {
         self.setTitleColor(.gray4, for: .normal)
+    }
+}
+
+final class PrimaryButton: TagButton {
+    
+    convenience init(title: String, isArrow: Bool) {
+        self.init(title: title)
+        setUI()
+        if isArrow {
+            self.semanticContentAttribute = .forceRightToLeft
+            let spacing: CGFloat = 2.0
+            self.titleEdgeInsets = UIEdgeInsets(top: 0, left: -spacing, bottom: 0, right: spacing)
+            self.imageEdgeInsets = UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: -spacing)
+            if let arrowImage = UIImage(systemName: "arrow.right") {
+                self.setImage(arrowImage, for: .normal)
+            }
+            self.sizeToFit()
+        }
+    }
+    
+    private func setUI() {
+        self.setTitleColor(.primaryMid, for: .normal)
+        self.tintColor = .primaryMid
+        self.layer.borderWidth = 1
+        self.layer.borderColor = CGColor(red: 255 / 255, green: 126 / 255, blue: 141 / 255, alpha: 1)
     }
 }
--- dormitoryFamiles/dormitoryFamiles/chat/chattingHomeViewController.swift
+++ dormitoryFamiles/dormitoryFamiles/chat/chattingHomeViewController.swift
@@ -10,10 +10,13 @@ import SnapKit
 
 class chattingHomeViewController: UIViewController {
     
+    let button = PrimaryButton(title: "전체보기", isArrow: true)
    
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavigationBar()
+//        addComponents()
+        setConstraints()
     }
     
     private func setNavigationBar() {
@@ -39,8 +42,22 @@ class chattingHomeViewController: UIViewController {
     @objc func logoButtonTapped() {
         print("로고 버튼 눌림")
     }
+//
+//    private func addComponents() {
+//        view.addSubview(navigationStackView)
+//        [logoImageView, navigationTitleLabel, searchButton].forEach{
+//            navigationStackView.addArrangedSubview($0) }
+//    }
     
-   
+    
+    private func setConstraints() {
+        view.addSubview(button)
+        button.snp.makeConstraints {
+            $0.top.equalTo(500)
+            $0.leading.equalTo(100)
+        }
+        
+    }
     
     
 }
