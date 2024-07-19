1515b9473575c3a5604207d1bc6b8e2f09c019f0
Haena <ghkwlsspdl@naver.com>
2024-07-19 21:35:51 +0900

    [#73] FEAT: 텍스트필드 델리게이트 설정
    - return누를시 어떤 로직 실행 될지 주석처리

--- dormitoryFamiles/dormitoryFamiles/chat/SearchChat/SearchTextFieldChattingViewController.swift
+++ dormitoryFamiles/dormitoryFamiles/chat/SearchChat/SearchTextFieldChattingViewController.swift
@@ -8,29 +8,28 @@
 import UIKit
 
 class SearchTextFieldChattingViewController: UIViewController {
-
     
     private let navigationTextField: UITextField = {
-           let textField = UITextField()
-           textField.placeholder = "검색어를 입력해주세요."
-           textField.font = .subTitle2
-           textField.backgroundColor = .clear
-           textField.borderStyle = .none
-           textField.isUserInteractionEnabled = true
-           return textField
-       }()
+        let textField = UITextField()
+        textField.placeholder = "검색어를 입력해주세요."
+        textField.font = .subTitle2
+        textField.backgroundColor = .clear
+        textField.borderStyle = .none
+        textField.isUserInteractionEnabled = true
+        return textField
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         setNavigationBar()
-        
+        setDelegate()
     }
     
     override func viewDidAppear(_ animated: Bool) {
-            super.viewDidAppear(animated)
-            navigationTextField.becomeFirstResponder()
-        }
+        super.viewDidAppear(animated)
+        navigationTextField.becomeFirstResponder()
+    }
     
     private func setNavigationBar() {
         let containerView = RoundLabel()
@@ -50,5 +49,17 @@ class SearchTextFieldChattingViewController: UIViewController {
             make.height.equalTo(40)
         }
     }
+    
+    private func setDelegate() {
+        navigationTextField.delegate = self
+    }
+}
 
+extension SearchTextFieldChattingViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //리턴 버튼이 눌린다면
+        //이전 화면의 keyword변경
+        //이전화면으로 돌아가면서 willAppear 실행시키도록
+        return true
+    }
 }
