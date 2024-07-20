aeeb72fd868af2eaf18bf0503b5a6793a13e0f2a
Haena <ghkwlsspdl@naver.com>
2024-07-21 00:46:35 +0900

    [#75] DESIGN: 네비게이션바 more버튼 생성
    - more이미지 추가
    - 네비게이션 오른쪽버튼아이템에 모어버튼 추가
    - 버튼 누를시 메서드 호출되도록 설정

--- dormitoryFamiles/dormitoryFamiles/chat/ChattingRoom/ChattingDetailViewController.swift
+++ dormitoryFamiles/dormitoryFamiles/chat/ChattingRoom/ChattingDetailViewController.swift
@@ -9,17 +9,18 @@ import UIKit
 
 class ChattingDetailViewController: UIViewController, ConfigUI {
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
         setNavigationBar()
         addComponents()
         setConstraints()
     }
     
     private func setNavigationBar() {
-        
+      
+        let moreImage = UIImage(named: "chattingDetailMore")?.withRenderingMode(.alwaysOriginal)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: moreImage, style: .plain, target: self, action: #selector(moreButtonTapped))
     }
     
     func addComponents() {
@@ -28,6 +29,10 @@ class ChattingDetailViewController: UIViewController, ConfigUI {
     
     func setConstraints() {
         
+    }
+    
+    @objc func moreButtonTapped() {
+        dump("moreButtonTapped")
     }
 
 }
