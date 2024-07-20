545c77c324081fdd2be9e0ba38f3b83bcf1e0429
Haena <ghkwlsspdl@naver.com>
2024-07-21 00:28:08 +0900

    [#75]  ADD: ChattingDetailViewController 생성
    - ConfigUI 채택

---
+++ dormitoryFamiles/dormitoryFamiles/chat/ChattingRoom/ChattingDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ChattingDetailViewController.swift
+//  dormitoryFamiles
+//
+//  Created by leehwajin on 2024/07/21.
+//
+
+import UIKit
+
+class ChattingDetailViewController: UIViewController, ConfigUI {
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addComponents()
+        setConstraints()
+    }
+    
+    func addComponents() {
+        
+    }
+    
+    func setConstraints() {
+        
+    }
+
+}
