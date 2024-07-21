b4516159b6e424245d6f0ae4aee187bb786d0cc0
Haena <ghkwlsspdl@naver.com>
2024-07-21 17:56:18 +0900

    [#75] DESIGN: MyChatting셀 완성
    - 피그마UI와 동일하게 설정
    - 원 이미지 추가
    - 날짜 포멧 변경 메서드 추가
    - 버그: 메세지가 10줄 이상일 경우 ...으로 처리됨

---
+++ dormitoryFamiles/dormitoryFamiles/chat/ChattingRoom/View/MyChattingTableViewCell.swift
@@ -0,0 +1,83 @@
+//
+//  MyChattingTableViewCell.swift
+//  dormitoryFamiles
+//
+//  Created by leehwajin on 2024/07/21.
+//
+
+import UIKit
+import SnapKit
+import Kingfisher
+
+class MyChattingTableViewCell: UITableViewCell, ConfigUI {
+
+    let cirleView = UIImageView(image: UIImage(named: "chattingDetailCircle"))
+    let messageLabel: RoundLabel = {
+        let label = RoundLabel(top: 8, left: 16, bottom: 8, right: 16)
+        label.layer.cornerRadius = 28
+        label.backgroundColor = .primaryMid
+        label.textColor = .white
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .lightGray
+        label.font = FontManager.small1()
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addComponents()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addComponents() {
+        contentView.addSubview(messageLabel)
+        contentView.addSubview(timeLabel)
+        contentView.addSubview(cirleView)
+    }
+    
+    func setConstraints() {
+        cirleView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.height.width.equalTo(7.8)
+        }
+        
+        messageLabel.snp.makeConstraints {
+            $0.top.equalTo(cirleView)
+            $0.leading.greaterThanOrEqualToSuperview().offset(50)
+            $0.trailing.equalTo(cirleView.snp.leading)
+            $0.bottom.equalToSuperview()
+        }
+        
+        timeLabel.snp.makeConstraints {
+            $0.trailing.lessThanOrEqualTo(messageLabel.snp.leading).inset(-8)
+            $0.bottom.equalToSuperview()
+            $0.leading.equalToSuperview()
+        }
+    }
+    
+    func configure(with message: ChatMessage) {
+        messageLabel.text = message.chatMessage
+        timeLabel.text = formatTime(message.sentTime)
+    }
+    
+    func formatTime(_ sentTime: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        if let date = dateFormatter.date(from: sentTime) {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "a hh:mm"
+            return formatter.string(from: date)
+        }
+        return sentTime
+    }
+}
