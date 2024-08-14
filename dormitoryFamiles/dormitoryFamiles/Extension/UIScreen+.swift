c0e94af8c79a3d6a89cdbcd460f92ff2ab62c8ed
BAE <lexon1209@gmail.com>
2024-08-15 07:37:57 +0900

    [#58] CHORE: 상수명 변경

--- dormitoryFamiles/dormitoryFamiles/Extension/UIScreen+.swift
+++ dormitoryFamiles/dormitoryFamiles/Extension/UIScreen+.swift
@@ -16,7 +16,7 @@ extension UIScreen {
     /// 직사각형 셀이 3열일 때 Width 값으로 사용합니다.
     static let cellWidth3Column: Int = Int(UIScreen.main.bounds.width - 56) / 3
     /// 원형 셀의 지름입니다. Width와 Height 값으로 사용할 수 있습니다.
-    static let circleCellRadius: Int = Int(UIScreen.main.bounds.width - 86) / 4
+    static let circleCellDiameter: Int = Int(UIScreen.main.bounds.width - 86) / 4
     /// 직사각형 셀의 높이입니다.
     static let cellHeight: Int = 48
 }
