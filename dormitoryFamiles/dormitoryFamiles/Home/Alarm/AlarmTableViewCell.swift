8d1d92e0b414ccdbd576a2bef978c746a6f24d3a
Haena <ghkwlsspdl@naver.com>
2024-08-30 02:55:37 +0900

    [#100] DESIGN: 테이블뷰 레이아웃
    - 임의의 셀로 레이아웃만 잡은 상태

---
+++ dormitoryFamiles/dormitoryFamiles/Home/Alarm/AlarmTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AlarmTableViewCell.swift
+//  dormitoryFamiles
+//
+//  Created by leehwajin on 8/30/24.
+//
+
+import UIKit
+
+class AlarmTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- dormitoryFamiles/dormitoryFamiles/Home/Alarm/AlarmViewController.swift
+++ dormitoryFamiles/dormitoryFamiles/Home/Alarm/AlarmViewController.swift
@@ -8,22 +8,58 @@
 import UIKit
 
 class AlarmViewController: UIViewController, ConfigUI {
-
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.separatorStyle = .none
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        setTableView()
         addComponents()
         setConstraints()
         self.navigationController?.isNavigationBarHidden = false
         setupNavigationBar("알림")
     }
     
+    private func setTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(AlarmTableViewCell.self, forCellReuseIdentifier: AlarmTableViewCell.identifier)
+    }
+    
     func addComponents() {
-        
+        view.addSubview(tableView)
     }
     
     func setConstraints() {
-        
+        tableView.snp.makeConstraints {
+            $0.top.bottom.equalTo(view.safeAreaLayoutGuide).inset(24)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
     }
-
 }
+
+extension AlarmViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: AlarmTableViewCell.identifier, for: indexPath) as? AlarmTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
+}
+
+extension AlarmViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(indexPath.row)번째 셀이 눌렸다")
+    }
+}
+
