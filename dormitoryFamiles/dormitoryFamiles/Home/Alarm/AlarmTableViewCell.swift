0b047ee433e8eb4d52bbfb0421733951677dde59
Haena <ghkwlsspdl@naver.com>
2024-08-30 02:57:03 +0900

    [#100] ADD: 알람 셀 생성 및 컴포넌트 정의

--- dormitoryFamiles/dormitoryFamiles/Home/Alarm/AlarmTableViewCell.swift
+++ dormitoryFamiles/dormitoryFamiles/Home/Alarm/AlarmTableViewCell.swift
@@ -6,18 +6,53 @@
 //
 
 import UIKit
+import SnapKit
 
-class AlarmTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+class AlarmTableViewCell: UITableViewCell, ConfigUI {
+    static let identifier = "alarmCell"
+    var roundBaseView = UIView()
+    var tabImage = UIImageView()
+    var tabTextLabel = UILabel()
+    var timeLabel = UILabel()
+    var descriptionLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUI()
+        addComponents()
+        setConstraints()
     }
-
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        addComponents()
+        setConstraints()
+    }
+    
+    //셀과의 간격을 15로 주었음
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: 15, right: 0))
+    }
+    
+    private func setUI() {
+        roundBaseView.layer.cornerRadius = 16
+        roundBaseView.layer.borderWidth = 1
+        roundBaseView.layer.borderColor = UIColor.gray1?.cgColor
+    }
+    
+    func addComponents() {
+        [roundBaseView, tabImage, tabTextLabel, timeLabel, descriptionLabel].forEach{ contentView.addSubview($0) }
+    }
+    
+    func setConstraints() {
+        roundBaseView.snp.makeConstraints {
+            $0.top.bottom.leading.trailing.equalTo(contentView.safeAreaLayoutGuide)
+        }
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
     }
-
 }
