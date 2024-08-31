2e20d70206454fd35ac87ef3e5e9f469e1a5e747
Haena <ghkwlsspdl@naver.com>
2024-09-01 01:44:15 +0900

    [#100] FEAT: 더미데이터 테스트
    - 알림 데이터가 잘 그려지는지 테스트
    - descriptionLabel의 높이에 따라 테이블뷰 셀 높이 동적 조정

--- dormitoryFamiles/dormitoryFamiles/Home/Alarm/AlarmTableViewCell.swift
+++ dormitoryFamiles/dormitoryFamiles/Home/Alarm/AlarmTableViewCell.swift
@@ -17,12 +17,8 @@ class AlarmTableViewCell: UITableViewCell, ConfigUI {
     var isRead = false
     var notificationId = 0
     var targetId = 0
-    var tabImageView = UIImageView()
-    var typeUrl: String? {
-        didSet {
-            updateProfileImage()
-        }
-    }
+    var typeImageView = UIImageView()
+
     private let roundBaseView: UIView = {
         let view = UIView()
         view.layer.cornerRadius = 16
@@ -40,7 +36,19 @@ class AlarmTableViewCell: UITableViewCell, ConfigUI {
         label.textColor = UIColor(red: 0.62, green: 0.624, blue: 0.631, alpha: 1)
         return label
     }()
-    var descriptionLabel = UILabel()
+    var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.font = .title4
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.adjustsFontSizeToFitWidth = false
+        label.minimumScaleFactor = 1.0
+        label.setContentCompressionResistancePriority(.required, for: .vertical)
+        label.setContentHuggingPriority(.required, for: .vertical)
+        return label
+    }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -55,10 +63,22 @@ class AlarmTableViewCell: UITableViewCell, ConfigUI {
         setConstraints()
     }
     
+    
+    
     //셀과의 간격을 15로 주었음
     override func layoutSubviews() {
         super.layoutSubviews()
+        contentView.layoutIfNeeded()
+        descriptionLabel.preferredMaxLayoutWidth = descriptionLabel.frame.width
         contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: 15, right: 0))
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        typeImageView.image = nil
+        typeLabel.text = nil
+        createdAtLabel.text = nil
+        descriptionLabel.text = nil
     }
     
     private func setUI() {
@@ -66,35 +86,56 @@ class AlarmTableViewCell: UITableViewCell, ConfigUI {
     }
     
     func addComponents() {
-        [roundBaseView, tabImageView, typeLabel, createdAtLabel, descriptionLabel].forEach{ contentView.addSubview($0) }
+        contentView.addSubview(roundBaseView)
+        [typeImageView, typeLabel, createdAtLabel, descriptionLabel].forEach{ roundBaseView.addSubview($0) }
     }
     
     func setConstraints() {
         roundBaseView.snp.makeConstraints {
-            $0.top.bottom.leading.trailing.equalTo(contentView.safeAreaLayoutGuide)
+            $0.top.leading.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
+
+        typeImageView.snp.makeConstraints {
+            $0.top.leading.equalToSuperview().inset(16)
+            $0.height.width.equalTo(20)
+        }
+
+        typeLabel.snp.makeConstraints {
+            $0.top.equalTo(typeImageView)
+            $0.leading.equalTo(typeImageView.snp.trailing).offset(4)
+        }
+
+        createdAtLabel.snp.makeConstraints {
+            $0.top.trailing.equalToSuperview().inset(16)
+        }
+
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(typeImageView.snp.bottom).offset(8).priority(.high)
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.bottom.equalToSuperview().inset(16).priority(.high)
+            $0.height.greaterThanOrEqualTo(25).priority(.required)
         }
     }
+
+
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        
-    }
-    
-    private func updateProfileImage() {
-        guard let typeUrl = typeUrl, let url = URL(string: typeUrl) else {
-            return
-        }
-        tabImageView.kf.setImage(with: url)
-        tabImageView.contentMode = .scaleAspectFill
     }
     
     func configure(articleTitle: String?, createdAt: String, isRead: Bool, notificationId: Int, sender: String, targetId: Int, type: String) {
         self.articleTitle = articleTitle ?? ""
         self.createdAtLabel.body2 = createdAt
+        //시간을 보고 몇분전 세팅
         self.isRead = isRead
         self.notificationId = notificationId
         self.sender = sender
         self.targetId = targetId
         self.typeLabel.button = type
+        //타입을 보고 description 분기처리
+        self.descriptionLabel.text = "암파인 괜차나 괜차나 딩딩디링딩\(type)"
+        //타입을 보고 이미지뷰 세팅(현재는 테스트 임시세팅)
+        typeImageView.image = UIImage(named: "chattingColor")
     }
 }
--- dormitoryFamiles/dormitoryFamiles/Home/Alarm/AlarmViewController.swift
+++ dormitoryFamiles/dormitoryFamiles/Home/Alarm/AlarmViewController.swift
@@ -18,6 +18,8 @@ class AlarmViewController: UIViewController, ConfigUI {
     private let tableView: UITableView = {
         let tableView = UITableView()
         tableView.separatorStyle = .none
+        tableView.estimatedRowHeight = 100 // 적당한 초기값 설정
+        tableView.rowHeight = UITableView.automaticDimension
         return tableView
     }()
     
@@ -72,45 +74,109 @@ class AlarmViewController: UIViewController, ConfigUI {
     }
     
     private func changeReadAlarm() {
-           let requestBody: [String: Any] = ["notificationIds": unreadId]
-           do {
-               let jsonData = try JSONSerialization.data(withJSONObject: requestBody, options: [])
-               Network.putMethod(url: Url.changeReadAlarm(), body: jsonData) { (result: Result<CodeResponse, Error>) in
-                   switch result {
-                   case .success(let successCode):
-                       print("PUT 성공: \(successCode)")
-                   case .failure(let error):
-                       print("Error: \(error)")
-                   }
-               }
-           } catch {
-               print("JSON 변환 에러: \(error)")
-           }
-    }
-    
-    private func getAlarmList(url: String) {
-        Network.getMethod(url: url) { (result: Result<NotificationsResponse, Error>) in
-            switch result {
-            case .success(let response):
-                self.alarmData += response.data.notifications
-                self.isLast = response.data.isLast
-                self.alarmData.filter{$0.isRead == false}.forEach{
-                    self.unreadId.append($0.notificationId)
+        let requestBody: [String: Any] = ["notificationIds": unreadId]
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: requestBody, options: [])
+            Network.putMethod(url: Url.changeReadAlarm(), body: jsonData) { (result: Result<CodeResponse, Error>) in
+                switch result {
+                case .success(let successCode):
+                    print("PUT 성공: \(successCode)")
+                case .failure(let error):
+                    print("Error: \(error)")
                 }
-                DispatchQueue.main.sync {
-                    self.tableView.reloadData()
-                }
-                self.isLoading = false
-                self.page += 1
-                print(response)
-            case .failure(let error):
-                print("Error: \(error)")
-                self.isLoading = false
             }
+        } catch {
+            print("JSON 변환 에러: \(error)")
         }
     }
     
-    private func alarmLoadNextPage() {
+    //    private func getAlarmList(url: String) {
+    //        Network.getMethod(url: url) { (result: Result<NotificationsResponse, Error>) in
+    //            switch result {
+    //            case .success(let response):
+    //                self.alarmData += response.data.notifications
+    //                self.isLast = response.data.isLast
+    //                self.alarmData.filter{$0.isRead == false}.forEach{
+    //                    self.unreadId.append($0.notificationId)
+    //                }
+    //                DispatchQueue.main.sync {
+    //                    self.tableView.reloadData()
+    //                }
+    //                self.isLoading = false
+    //                self.page += 1
+    //                print(response)
+    //            case .failure(let error):
+    //                print("Error: \(error)")
+    //                self.isLoading = false
+    //            }
+    //        }
+    //    }
+    
+    //아직 내 계정에 알림 목록이 없어서, 목데이터 확인용
+    private func getAlarmList(url: String) {
+        // 더미 데이터
+        let dummyData = NotificationsResponse(
+            code: 200,
+            data: NotificationsDataClass(
+                isLast: false,
+                notifications: [
+                    NotificationData(
+                        notificationId: 68,
+                        type: "ARTICLE_WISH",
+                        sender: "바이오헬스천재",
+                        articleTitle: "바퀴벌레 잡아주실 분",
+                        isRead: false,
+                        targetId: 58,
+                        createdAt: "2024-08-20T17:04:04"
+                    ),
+                    NotificationData(
+                        notificationId: 67,
+                        type: "ARTICLE_REPLY_COMMENT",
+                        sender: "바이오헬스천재",
+                        articleTitle: "바퀴벌레 잡아주실 분",
+                        isRead: false,
+                        targetId: 58,
+                        createdAt: "2024-08-20T17:01:50"
+                    ),
+                    NotificationData(
+                        notificationId: 66,
+                        type: "ARTICLE_COMMENT",
+                        sender: "바이오헬스천재",
+                        articleTitle: "바퀴벌레 잡아주실 분",
+                        isRead: false,
+                        targetId: 58,
+                        createdAt: "2024-08-20T17:00:02"
+                    ),
+                    NotificationData(
+                        notificationId: 64,
+                        type: "CHAT",
+                        sender: "농생명천재",
+                        articleTitle: nil,
+                        isRead: false,
+                        targetId: 1,
+                        createdAt: "2024-08-20T16:46:01"
+                    )
+                ]
+            )
+        )
+        
+        // 데이터 처리
+        self.alarmData += dummyData.data.notifications
+        self.isLast = dummyData.data.isLast
+        self.alarmData.filter { $0.isRead == false }.forEach {
+            self.unreadId.append($0.notificationId)
+        }
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+        
+        self.isLoading = false
+        self.page += 1
+    }
+    
+    
+    private func loadNextPage() {
         guard !isLast else { return }
         page += 1
         getAlarmList(url: Url.alarmList(page: page, size: nil))
@@ -139,17 +205,15 @@ extension AlarmViewController: UITableViewDataSource {
         cell.selectionStyle = .none
         return cell
     }
-    
-    private func loadNextPage() {
-        guard !isLast else { return }
-        page += 1
-        getAlarmList(url: Url.alarmList(page: page, size: nil))
-    }
 }
 
 extension AlarmViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("\(indexPath.row)번째 셀이 눌렸다")
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
     }
 }
 
